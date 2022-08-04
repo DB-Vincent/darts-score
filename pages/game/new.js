58041a7af14a314249f681d5eaaa4935a6d605bd
DB-Vincent <vincent.deborger@gmail.com>
2022-08-04 12:04:40 +0200

    Make use of react-hook-form

--- pages/game/new.js
+++ pages/game/new.js
@@ -2,13 +2,16 @@ import Head from 'next/head';
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Link from 'next/link';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
+import { useForm } from "react-hook-form";
 
 export default function Create() {
     const [userData, setUserData] = useState();
     const [loading, setLoading] = useState(true);
-    const [players, setPlayers] = useState([])
-    const router = useRouter()
+    const [players, setPlayers] = useState([]);
+    const [checkedItems, setCheckedItems] = useState({});
+    const router = useRouter();
+    const { handleSubmit, watch, formState: { errors } } = useForm();
 
     useEffect(() => {
         axios.get("/api/user")
@@ -18,30 +21,46 @@ export default function Create() {
         });
     });
 
-    const handleOnChange = (player) => {
+    const Checkbox = ({ type = "checkbox", name, checked = false, onChange }) => {      
+        return (
+            <input type={type} name={name} checked={checked} onChange={onChange} />
+        );
+    };
+
+    const handleChange = event => {
+        setCheckedItems({
+          ...checkedItems,
+          [event.target.name]: event.target.checked
+        });
+
         let tempPlayers = players;
 
-        if (tempPlayers.includes(player)) {
-            tempPlayers = tempPlayers.filter((playerName) => playerName !== player);
+        if (tempPlayers.includes(event.target.name)) {
+            tempPlayers = tempPlayers.filter((playerName) => playerName !== event.target.name);
             setPlayers(tempPlayers);
         } else {
-            tempPlayers.push(player);
+            tempPlayers.push(event.target.name);
             setPlayers(tempPlayers)
         }
+
+        console.log(tempPlayers)
     };
-    
-    const createGame = async (event) => {
-        event.preventDefault();
+
+    const createGame = async () => {   
+        if (players.length === 2) {
+            axios.post('/api/game', {
+                player1: Number(players[0]),
+                player2: Number(players[1]),
+                score: 501
+            })
+            .then((res) => {
+                router.push(`/game/${res.data.id}`)
+                console.log(res)
+            });
+        } else {
+            console.log("Not enough or too many players.")
+        }
         
-        axios.post('/api/game', {
-            player1: Number(players[0]),
-            player2: Number(players[1]),
-            score: 501
-        })
-        .then((res) => {
-            router.push(`/game/${res.data.id}`)
-            console.log(res)
-        });
     };
 
     return (
@@ -54,17 +73,23 @@ export default function Create() {
             <h1 className="text-3xl font-bold">New user creation</h1>
             <Link href="/">Home</Link>
             <br></br>
-            {loading ? <p>Loading users..</p> : <form onSubmit={createGame}>
+            {loading ? <p>Loading users..</p> : <form onSubmit={handleSubmit(createGame)}>
                 {userData.map((user) => {
                     return (
                         <>
-                            <input id="name" name="name" value={user.name} type="checkbox" onChange={() => handleOnChange(user.id)}/> 
-                            <label htmlFor="name"> {user.name}</label><br></br>
+                            <label key={user.id}>
+                                {user.name}
+                                <Checkbox
+                                    name={user.id}
+                                    checked={checkedItems[user.id]}
+                                    onChange={handleChange}
+                                />
+                            </label>
                         </>
                     )
                 })}
                 
-                <button type="submit">Create</button>
+                <input type="submit" />
             </form>}
             
         </div>
