94453c17e104d128b9a2915949f7f1d08ab22184
DB-Vincent <vincent.deborger@gmail.com>
2022-08-06 15:14:56 +0200

    Fix index issue

--- components/GameList.js
+++ components/GameList.js
@@ -19,15 +19,16 @@ const GameList = () => {
             <h2 className="text-2xl font-bold mb-4">Games</h2>
             { loading 
             ? <p className="w-full text-center text-slate-400">Loading games..</p> 
-            : <ul className="list-decimal">
+            : <ul className="list-none">
                 { gameData.length != 0
                 ? <>
                     { gameData.map((game) => {
+                        console.log(game)
                         return (
                             <li key={game.id}>
                                 {game.winner 
                                 ? <p><span className="text-medium">{game.player1.name}</span> vs <span className="text-medium">{game.player2.name}</span>: {gameData.winner.name} won!</p>
-                                : <Link href={"/game/" + game.id}><span className="text-medium">{game.player1.name}</span> vs <span className="text-medium">{game.player2.name}</span>: {game.scorePlayer1}/{game.scorePlayer2}</Link>}
+                                : <a href={"/game/" + game.id}><span className="text-medium">{game.player1.name}</span> vs <span className="text-medium">{game.player2.name}</span>: {game.scorePlayer1}/{game.scorePlayer2}</a>}
                             </li>
                             
                         )
